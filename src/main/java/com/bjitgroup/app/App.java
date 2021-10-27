82481f27f8eb87d96f3e34d9e4b2bd859424e6b7
Md. Tanvir Ahmed <md.tanvir@bjitgroup.com>
2021-10-27 16:56:27 +0600

    first commit of project-one

---
+++ src/main/java/com/bjitgroup/app/App.java
@@ -0,0 +1,13 @@
+package com.bjitgroup.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
